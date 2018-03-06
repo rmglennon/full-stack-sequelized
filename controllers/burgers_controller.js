886f79dfbac341847406a8e69b7be8d531870dfe
Rhonda Glennon <rhondaglennon@gmail.com>
2018-03-05 21:28:27 -0800

    remove orm references and add sequelize init files

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -10,35 +10,17 @@ var burger = require("../models/burger.js");
 
 // get request to select all and render the page with the handlebars index
 router.get("/", function(req, res) {
-  burger.selectAll(function(data) {
-
-    var hbsObject = {
-      burgers: data
-    };
-    res.render("index", hbsObject);
-  });
+  
 });
 
 // post request to add a new burger and set its devoured value to false 
 router.post("/api/burgers", function(req, res) {
-  burger.insertOne(["burger_name", "devoured"], [
-    req.body.burger_name, false
-  ], function(result) {
-    res.json({ id: result.insertId });
-  });
+
 });
 
 // put request to update the burger that was clicked in the list to be devoured: true
 router.put("/api/burgers/:id", function(req, res) {
-  var condition = "id = " + req.params.id;
-  burger.updateOne({
-    devoured: req.body.devoured
-  }, condition, function(result) {
-    if (result.changedRows == 0) {
-      return res.status(404).end();
-    } else {
-      res.status(200).end();
-    }
+
   });
 });
 
